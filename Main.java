77b909abf7d0f3199b4e93f3c321a6a21439193f
mathemaia <matheusmaiadasilva@gmail.com>
2022-10-30 17:35:36 -0300

    update

--- Main.java
+++ Main.java
@@ -1,8 +1,35 @@
-import Dados.BancoDeDados;
+import Listas.ListaDeAcidentes;
+import Listas.ListaDeRuas;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BancoDeDados b = new BancoDeDados();
-        b.hello();
+        ListaDeRuas ruas = new ListaDeRuas();
+
+        for (int i = 0; i < 3; i++) {
+            String nome = "Rua - " + i;
+            // Cria uma lista de acidentes.
+            ListaDeAcidentes acidentes = new ListaDeAcidentes(nome);
+
+            // Adiciona acidentes na lista de acidentes.
+            for (int j = 0; j < 4; j++) {
+                acidentes.addAcidente(j, (i + "/" + i + "/" + i), (i + ":00"), i, null, null, null, i, i, i, i,
+                        i, i, i, i, i, i, i, null, null, null, null);
+            }
+
+            // Adiciona uma rua a lista de ruas com uma lista de acidentes.
+            ruas.add(nome, acidentes);
+            ruas.getRua(i);
+        }
+
+
+        for (int i = 0; i < ruas.size(); i++) {
+            System.out.println(ruas.getRua(i) );
+            System.out.println("    " + ruas.getAcidentes(i));
+            for (int j = 0; j < ruas.getAcidentes(i).size(); j++) {
+                System.out.println("        " + ruas.getAcidentes(i).getAcidente(j));
+            }
+        }
+
     }
 }
